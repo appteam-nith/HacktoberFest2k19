95f82bb81fe457f5fd3d7165971492aa077183c8
yourdad285 <arnabsaha285@gmail.com>
2019-10-20 15:04:50 +0530

    Removed * from gitignore added arnab's file

---
+++ C++/arnab194536.cpp
@@ -0,0 +1,16 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{ printf("Heyyy, What's up?");
+printf("heeeeyyyyyy, nothing, what about you ");
+printf("everything, what to tell");
+printf("tell me about the most interesting thing");
+printf("voldemont: knock knock  ");
+printf("harry: who's there  ");
+printf("voldemont: you know  ");
+printf("harry: you know who  ");
+printf("voldemont: correct!  ");
+
+
+return 0;
+}
