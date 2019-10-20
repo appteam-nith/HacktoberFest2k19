46f553c4df873663781d5cf26a13a12a16fee57f
Aalok766626 <55963268+Aalok766626@users.noreply.github.com>
2019-10-20 15:31:44 +0530

    Reverse a string

---
+++ C/array11rev.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main(){
+	int a[5],i,n;
+	printf("Enter the number\n");
+	scanf("%d",&n);
+	for(i=4;i>=0;i--)
+	{a[i]=n%10;
+		n/=10;}
+		printf("Reversed no. is=\n");
+	for(i=4;i>=0;i--)
+	{printf("%d",a[i]);}
+	}
