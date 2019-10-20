baa52ca8d4ab138fb48e6aa171807b4e7076e005
Anubhav10052001 <anubhav2001kumarrao@gmail.com>
2019-10-20 16:07:10 +0530

    added rao

---
+++ C/rao.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the value of a");
+	scanf("%d",&a);
+	printf("enter the value of b");
+	scanf("%d",&b);
+	c=a%b;
+	printf("%d\n",c);
+	getch();
+	
+}
