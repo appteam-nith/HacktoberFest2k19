55899145c47d0055cde37bea9373dfd4b65a7af6
KriTiCal007 <danyal.kritik@gmail.com>
2019-10-20 15:39:31 +0530

    Add ed decimal to binary converter

--- C/DecimalToBinaryConverter195018.c
+++ C/DecimalToBinaryConverter195018.c
@@ -1,19 +1,19 @@
-#include<iostream>
-
-using namespace std;
+#include<stdio.h>
 int main(int argc, char const *argv[])
 {
     int a , arr[32] , i=0;
-    cout<<"Enter any decimal number : ";
-    cin>>a;
+    printf("Enter any decimal number : ");
+    scanf("%d",&a);
     while(a>0)
     {
         arr[i] = a%2;
         a = a/2;
         i++;
     }
-    cout<<"The Binary form is : ";
+    printf("The Binary form is : ");
     for(int j=i-1; j>=0 ; j--)
-      cout<<arr[j];
+    {
+      printf("%d",arr[j]);
+    } 
     return 0;
 }
