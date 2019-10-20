2b78a4d98d9a7a1fcf514614ca610ec773de19cc
Garima-Vasanta <garimavasanta@gmail.com>
2019-10-20 15:00:38 +0530

    added file191072

---
+++ C/191072.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{ int a,b,rem,Q;
+printf("Enter DIVIDENT a:");
+scanf("%d",&a);
+printf("Enter DIVISOR b:");
+scanf("%d",&b);
+Q=a/b;
+rem=a%b;
+printf("The Quotient is:%d",Q);
+printf("The remainder is:%d",rem);
+return 0;
+
+
+
+}
