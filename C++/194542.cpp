9de6fb1ca152bd4560f15b2b074bbd6247ccda26
Rishabh11072001 <agarwaldindyal11072001@gmail.com>
2019-10-20 15:33:39 +0530

    AP Program has been added

--- C++/194542.cpp
+++ C++/194542.cpp
@@ -1,15 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-	int i, j, n;
-	cin>>n;
-for(i=1; i<=n; i++)
-{
-	for(j=1; j<=i; j++)
-		{cout<<"*";}
-
-cout<<endl;
+	int n, a, d, An=0, Sn=0;
+cout<<"Value of A = ";
+cin>>a;
+cout<<"Value of D = ";
+cin>>d;
+cout<<"Number of Terms = ";
+cin>>n;
+	
+	for(int i=1; i<=n; i++)
+	{
+	An=(a+((i-1)*d));
+	cout<<"Term "<<i<<" = "<<An<<endl;
+Sn=Sn+An;
 }
-	return 0;
+	cout<<"Sum = "<<Sn<<endl;
+return 0;
 }
