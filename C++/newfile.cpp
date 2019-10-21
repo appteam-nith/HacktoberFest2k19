42a60c12b45e4e94b8c55b2e1650205758a065e8
vaibhavpathak999 <vaibhavpathak999@gmail.com>
2019-10-21 19:46:30 +0530

    new changes

---
+++ C++/newfile.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+
+int main()
+{
+clrscr();
+int x,y,avg=0;
+cout<<"Enter two numbers : ";
+Cin>>x;
+cin>>y;
+avg=(x+y)/2
+cout<<"The average of entered numbers is :"<<avg;
+getch();
+return 0;
+}
