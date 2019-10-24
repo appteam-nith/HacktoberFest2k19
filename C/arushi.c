161f75bf5469a552c387851a01e34a84a2fb96d9
arushijain-20 <arushijain0202@gmail.com>
2019-10-24 22:13:50 +0530

    changes are added

---
+++ C/arushi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,average;
+	printf("enter two numbers:\n");
+	scanf("%d%d",&a,&b);
+	average=(a+b)/2;
+	printf("average=%d",average);
+	return 0;
+}
