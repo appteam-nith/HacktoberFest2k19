eced86c1e86291aeab01793a0e1a821aa1f2d003
Sahil2607-hub <sahilchaudhary26@gmail.com>
2019-10-26 10:59:27 +0530

    2nd c++ file is added

---
+++ C++/ifcode.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int i =15;
+if(i>10)
+{
+	cout<<"Greater than 10";
+}
+else if(i>20)
+{
+	cout<<"Greater than 20";
+}
+else
+{
+	cout<<"Greater";
+}
+return 0;
+}
