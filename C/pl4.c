3d336b80aa117c7fde6979d58c2c31df16ec8421
Garima-Vasanta <garimavasanta@gmail.com>
2019-10-27 14:13:30 +0530

    added filepl4

---
+++ C/pl4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{ int a,b,c,d,e,product;
+printf("enter the number a:");
+scanf("%d",&a);
+printf("enter the number b:");
+scanf("%d",&b);
+printf("enter the number c:");
+scanf("%d",&c);
+printf("enter the number d:");
+scanf("%d",&d);
+printf("enter the number e:");
+scanf("%d",&e);
+product=a*b*c*d*e;
+printf("the product is:%d",product);
+return 0;
+}
